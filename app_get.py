da4581a8065cde3012222d1d56e4d068b23cfc49
miguelsgomess19 <miguelsgomes13@gmail.com>
2026-03-19 11:19:50 -0300

    cliente servidor

---
+++ app_get.py
@@ -0,0 +1,19 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Servidor(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.end_headers()
+        self.wfile.write(b"Servidor funcionando com GET")
+        
+    def do_POST(self):
+
+        tamanho = int(self.headers['Content-Length'])
+        dados = self.rfile.read(tamanho)
+        print("Dados recebidos:", dados.decode())
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"POST recebido")
+
+
+
+HTTPServer(("0.0.0.0", 8000), Servidor).serve_forever()
